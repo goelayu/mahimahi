2d02a8c0089f4762bc38bdf48661ec3dc6fea3d3
goelayu@umich.edu <goelayu@umich.edu>
2020-10-05 19:09:26 -0400

    A no-op shell which simply creates a network namespace

---
+++ src/frontend/noop.cc
@@ -0,0 +1,81 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <net/route.h>
+#include <fcntl.h>
+
+#include <vector>
+#include <set>
+
+#include "util.hh"
+#include "netdevice.hh"
+#include "web_server.hh"
+#include "system_runner.hh"
+#include "socket.hh"
+#include "event_loop.hh"
+#include "temp_file.hh"
+#include "http_response.hh"
+#include "dns_server.hh"
+#include "exception.hh"
+
+#include "http_record.pb.h"
+
+#include "config.h"
+
+using namespace std;
+
+void add_dummy_interface( const string & name, const Address & addr )
+{
+    run( { IP, "link", "add", name, "type", "dummy" } );
+
+    interface_ioctl( SIOCSIFFLAGS, name,
+                     [] ( ifreq &ifr ) { ifr.ifr_flags = IFF_UP; } );
+    interface_ioctl( SIOCSIFADDR, name,
+                     [&] ( ifreq &ifr ) { ifr.ifr_addr = addr.to_sockaddr(); } );
+}
+
+int main( int argc, char *argv[] )
+{
+    try {
+        /* clear environment */
+        char **user_environment = environ;
+        environ = nullptr;
+
+        check_requirements( argc, argv );
+        
+        /* get working directory */
+        const string working_directory { get_working_directory() };
+
+        /* chdir to result of getcwd just in case */
+        SystemCall( "chdir", chdir( working_directory.c_str() ) );
+
+        /* what command will we run inside the container? */
+        vector< string > command;
+
+        command.push_back("bash");
+
+        /* create a new network namespace */
+        SystemCall( "unshare", unshare( CLONE_NEWNET ) );
+
+        /* bring up localhost */
+        interface_ioctl( SIOCSIFFLAGS, "lo",
+                         [] ( ifreq &ifr ) { ifr.ifr_flags = IFF_UP; } );
+
+        /* initialize event loop */
+        EventLoop event_loop;
+
+        event_loop.add_child_process( join( command ), [&]() {
+                drop_privileges();
+
+                /* restore environment and tweak bash prompt */
+                environ = user_environment;
+                prepend_shell_prefix( "[noop] " );
+
+                return ezexec( command, true );
+        } );
+
+        return event_loop.loop();
+    } catch ( const exception & e ) {
+        print_exception( e );
+        return EXIT_FAILURE;
+    }
+}
